1b0607a678b0540d7eccdaa45b0c8cabb672bd4b
Marcel <125443370+zdzimil@users.noreply.github.com>
2026-01-28 10:47:25 +0100

    testy
    Testy

---
+++ sql/testy_wydajnosci/Izolacja_oknoA.sql
@@ -0,0 +1,9 @@
+
+RESET ROLE;
+SET ROLE jan_klient;
+
+BEGIN;
+-- SPRAWDZ JAKIE ID MA JAN KLIENT
+CALL wykonaj_przelew(101, 2, 100, 'Test izolacji');
+
+SELECT 'JAN WIDZI:' as kto, saldo_bieżące FROM konta LIMIT 1;
---
+++ sql/testy_wydajnosci/Izolacja_oknoB.sql
@@ -0,0 +1,6 @@
+--Odpal w drugim terminalu
+RESET ROLE;
+SET ROLE ewa_szefowa;
+
+-- id janka to 1
+SELECT 'KIEROWNIK WIDZI:' as kto, saldo_bieżące FROM konta where id_klienta = 1;
